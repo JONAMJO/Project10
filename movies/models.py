8376cd97c6f35ca00a3492816fbdc0f37cad2410
Nam-SH <gtsmell@gmail.com>
2019-11-20 16:16:46 +0900

    model.py

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    audience = models.IntegerField()
+    poster_url = models.CharField(max_length=200)
+    description = models.TextField()
+    genre = models.ManyToManyField(Genre)
+
+
+class Review(models.Model):
+    content = models.TextField()
+    score = models.IntegerField()
+    movie = models.ManyToManyField(Movie)
+    user = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+
+    class Meta:
+        ordering = ('-pk',)
