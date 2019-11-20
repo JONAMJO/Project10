71762a737c57b2e9dc6ede2e55d590fbc08d1eb6
johh201200 <johh201200@gmail.com>
2019-11-20 17:55:10 +0900

    ad

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 from . import views
 
+app_name = 'movies'
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:movie_pk>/', views.detail, name='detail'),
+    path('<int:movie_pk>/delete/', views.delete, name='delete'),
+    path('<int:movie_pk>/update/', views.update, name='update'),
+    path('<int:movie_pk>/comments/', views.comments_create, name='comments_create'),
+    path(
+        '<int:movie_pk>/comments/<int:comment_pk>/delete/',
+        views.comments_delete,
+        name='comments_delete',
+    ),
+    path('<int:movie_pk>/like/', views.like, name='like'),
+    path('<int:movie_pk>/follow/<int:user_pk>/', views.follow, name='follow'),
+    path('<int:hash_pk>/hashtag/', views.hashtag, name='hashtag'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+import hashlib
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.http import Http404, HttpResponse
+from django.views.decorators.http import require_POST
+# from .models import Article, Comment, Hashtag
+# from .forms import ArticleForm, CommentForm
 
 # Create your views here.
+def index(request):
+    visits_num = request.session.get('visits_num', 0)
+    request.session['visits_num'] = visits_num + 1
+    request.session.modified = True
+    articles = Article.objects.all()
+    context = {'articles': articles, 'visits_num': visits_num,}
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    comments = article.comment_set.all()
+    person = get_object_or_404(get_user_model(), pk=article.user_id)
+    comment_form = CommentForm()
+    context = {'article': article, 'comment_form': comment_form, 'comments': comments, 'person': person,}
+    return render(request, 'articles/detail.html', context)
+
+
+@require_POST
+def comments_create(request, article_pk):
+    if request.user.is_authenticated:
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.article_id = article_pk
+            comment.user = request.user
+            comment.save()
+    return redirect('articles:detail', article_pk)
+
+
+@require_POST
+def comments_delete(request, article_pk, comment_pk):
+    if request.user.is_authenticated:
+        comment = get_object_or_404(Comment, pk=comment_pk)
+        if request.user == comment.user:
+            comment.delete()
+        return redirect('articles:detail', article_pk)
+    return HttpResponse('You are Unauthorized', status=401)
+
+
+@login_required
+def like(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+
+    if article.like_users.filter(pk=request.user.pk).exists():
+        article.like_users.remove(request.user)
+    else:
+        article.like_users.add(request.user)
+
+    return redirect('articles:index')
+
+
+@login_required
+def follow(request, article_pk, user_pk):
+    person = get_object_or_404(get_user_model(), pk=user_pk)
+    user = request.user
+    if person != user:
+        if person.followers.filter(pk=user.pk).exists():
+            person.followers.remove(user)
+        else:
+            person.followers.add(user)
+    return redirect('articles:detail', article_pk)
+
+
+def hashtag(request, hash_pk):
+    hashtag = get_object_or_404(Hashtag, pk=hash_pk)
+    articles = hashtag.article_set.order_by('-pk')
+    context = {'hashtag': hashtag, 'articles': articles,}
+    return render(request, 'articles/hashtag.html', context)
